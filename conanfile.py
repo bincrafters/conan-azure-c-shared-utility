57ac6c97d8721288cf5b8797fdd924616afd7073
Uilian Ries <uilianries@gmail.com>
2017-08-17 23:18:13 -0300

    Build project on Linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,38 +1,52 @@
-from conans import ConanFile, CMake, os
+from os import path
+from conans import ConanFile, CMake, tools
 
 
 class AzureCSharedUtilityConan(ConanFile):
-    name = "Azure-C-Utility"
-    version = "1.0.41"
-    generators = "cmake" 
+    name = "azure-c-shared-utility"
+    version = "2017-08-11"
+    generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/bincrafters/conan-azure-c-shared-utility"
-    source_url = "https://github.com/Azure/azure-c-shared-utility"
-    git_tag = "2017-08-11"
-    description = "Azure C SDKs common code"
     license = "https://github.com/Azure/azure-c-shared-utility/blob/master/LICENSE"
-    lib_short_name = "azure-c-shared-utility"
-        
+    description = "Azure C SDKs common code"
+    release_name = "%s-%s" % (name, version)
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    exports = ["LICENSE"]
+
     def source(self):
-        self.run("git clone --depth=1 --branch={0} {1}.git"
-                .format(self.git_tag, self.source_url)) 
+        tools.get("https://github.com/Azure/azure-c-shared-utility/archive/%s.tar.gz" % self.version)
+
+    def system_requirements(self):
+        # libcurl is required
+        if self.settings.os == "Linux":
+            package_tool = tools.SystemPackageTool()
+            package_tool.install(packages="libcurl4-gnutls-dev uuid-dev", update=True)
 
     def build(self):
+        conan_magic_lines = '''project(azure_c_shared_utility)
+    include(../conanbuildinfo.cmake)
+    conan_basic_setup()
+    '''
+        tools.replace_in_file("%s/CMakeLists.txt" % self.release_name, "project(azure_c_shared_utility)", conan_magic_lines)
         cmake = CMake(self)
-        cmake.configure(source_dir=self.lib_short_name, build_dir="./")
+        cmake.configure(source_dir=self.release_name)
         cmake.build()
-        
+
     def package(self):
-        include_dir = os.path.join(self.lib_short_name, "inc")
-        
-        config_dir = os.path.join(self.lib_short_name, "configs")
-                       
-        self.copy(pattern="*", dst="include", src=include_dir)
-        self.copy(pattern="*.lib", dst="lib", src="")
-        self.copy(pattern="azure_c_shared_utilityFunctions.cmake", dst="res", src=config_dir)
+        self.copy(pattern="LICENSE", dst=".", src=".")
+        self.copy(pattern="*", dst="include", src=path.join(self.release_name, "inc"))
+        self.copy(pattern="azure_c_shared_utilityFunctions.cmake", dst="res", src=path.join(self.release_name, "configs"))
+        self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=".", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*", dst="bin", src="bin", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["aziotsharedutil"]
-        self.cpp_info.libdirs = ["lib"]
-        self.cpp_info.resdirs = ["res"]
-
+        self.cpp_info.libs = self.collect_libs()
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("curl")
+            self.cpp_info.libs.append("uuid-dev")
