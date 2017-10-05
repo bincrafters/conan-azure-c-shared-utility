de75d1580267b28e452d02ecb0e30cf6baf826e6
Uilian Ries <uilianries@gmail.com>
2017-10-04 22:02:29 -0300

    Avoid to use parallel option on test_package
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,15 +7,15 @@ class TestPackageConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self, parallel=False)
         cmake.configure()
         cmake.build()
-        
+
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy("*.so*", dst="bin", src="lib")        
-        
+        self.copy("*.so*", dst="bin", src="lib")
+
     def test(self):
         os.chdir("bin")
         try:
