09dc60e8d519df7fa8edb2aa454204a76678ba8b
Jerry W <jerrywiltse@gmail.com>
2018-01-05 15:53:46 -0500

    refresh test_package and cmakelists wrapper

--- conanfile.py
+++ conanfile.py
@@ -1,58 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os import path
+import os
 from conans import ConanFile, CMake, tools
 
 
 class AzureCSharedUtilityConan(ConanFile):
     name = "azure-c-shared-utility"
     version = "1.0.49"
+    release_date = "2017-12-14"
     url = "https://github.com/bincrafters/conan-azure-c-shared-utility"
     description = "Azure C SDKs common code"
     license = "MIT"
     exports = ["LICENSE.md", "azure_c_shared_utilityConfig.cmake"]
+    exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
+    source_subfolder = "source_subfolder"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=True"
-    lib_short_name = "azure_c_shared_utility"
-    release_date = "2017-09-08"
-    release_name = "%s-%s" % (name.lower(), release_date)
-
+    requires = "libcurl/[>=7.56.1]@bincrafters/stable"
+    
     def source(self):
         source_url = "https://github.com/Azure/azure-c-shared-utility"
-        tools.get("%s/archive/%s.tar.gz" % (source_url, self.release_date))
-
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.release_date))
+        extracted_dir = self.name + "-" + self.release_date
+        os.rename(extracted_dir, self.source_subfolder)
+        
     def requirements(self):
         if self.settings.os == "Linux" or self.settings.os == "Macos":
             self.requires.add("OpenSSL/1.0.2l@conan/stable")
 
-    def system_requirements(self):
-        # libcurl and uuid are required on Linux
-        if self.settings.os == "Linux":
-            package_tool = tools.SystemPackageTool()
-            package_tool.install(packages="libcurl4-gnutls-dev uuid-dev pkg-config")
-
     def build(self):
-        conan_magic_lines = '''project(%s)
-    include(../conanbuildinfo.cmake)
-    conan_basic_setup()
-    ''' % self.lib_short_name
-
-        cmake_file = "%s/CMakeLists.txt" % self.release_name
-        tools.replace_in_file(cmake_file, "project(%s)" % self.lib_short_name, conan_magic_lines)
         cmake = CMake(self, parallel=False)
         cmake.definitions["skip_samples"] = True
         cmake.definitions["build_as_dynamic"] = self.options.shared
-        cmake.configure(source_dir=self.release_name)
+        cmake.configure()
         cmake.build()
 
     def package(self):
+        configs_folder = os.path.join(self.source_subfolder, "configs")
+        include_folder = os.path.join(self.source_subfolder, "inc")
         self.copy(pattern="LICENSE", dst=".", src=".")
-        self.copy(pattern="*", dst="include", src=path.join(self.release_name, "inc"))
+        self.copy(pattern="*", dst="include", src=include_folder)
         self.copy(pattern="azure_c_shared_utilityConfig.cmake", dst="res", src=".")
-        self.copy(pattern="azure_c_shared_utilityFunctions.cmake", dst="res", src=path.join(self.release_name, "configs"))
+        self.copy(pattern="azure_c_shared_utilityFunctions.cmake", dst="res", src=configs_folder)
         self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
         self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
